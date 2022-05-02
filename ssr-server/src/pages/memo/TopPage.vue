f26cd5b148d4536302d5563a438a4f91ab2e98be
aocm <aocm.dev@gmail.com>
2022-05-02 16:39:56 +0900

    一覧画面のレイアウトを変更

--- ssr-server/src/pages/memo/TopPage.vue
+++ ssr-server/src/pages/memo/TopPage.vue
@@ -1,59 +1,68 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
-    <el-table-column fixed="right" label="Operations" width="120">
-      <template #default="scope">
-        <el-button
-          type="text"
-          size="small"
-          @click.prevent="dialogVisible=true"
-        >
-          確認する
-        </el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-  <el-dialog
-    v-model="dialogVisible"
-    title="Tips"
-    width="30%"
-  >
-    <span>This is a message</span>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button type="primary" @click="dialogVisible = false"
-          >とじる</el-button
-        >
-      </span>
-    </template>
-  </el-dialog>
+  <el-container>
+    <el-aside width="40%">
+      <el-table 
+        :data="tableData" 
+        style="width: 100%"
+        @row-click="selectData"
+      >
+        <el-table-column prop="date" label="Date" width="100" />
+        <el-table-column prop="name" label="Name" width="100" />
+        <el-table-column prop="address" label="Address" />
+      </el-table>
+    </el-aside>
+    <el-main>
+      <el-card class="box-card">
+        <template #header>
+          <div class="card-header">
+            <span>{{selectedData.name}}</span>
+            <el-button class="button" type="text">Operation button</el-button>
+          </div>
+        </template>
+        {{selectedData}}
+      </el-card>
+    </el-main>
+  </el-container>
 </template>
-<script lang="ts" setup>
 
-import { ref } from 'vue'
-const dialogVisible = ref(false)
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import type { ElTable } from 'element-plus'
+const currentRow = ref()
+const singleTableRef = ref<InstanceType<typeof ElTable>>()
+
+const selectData= (data)=>{
+  selectedData.date = data.date
+  selectedData.name = data.name
+  selectedData.address = data.address
+}
+
+const selectedData = reactive({
+    date: "2016-05-03",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+})
+
 const tableData = [
   {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "2016-05-03",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "2016-05-02",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "2016-05-04",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "2016-05-01",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
   },
-]
+];
 </script>
