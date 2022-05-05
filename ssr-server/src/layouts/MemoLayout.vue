06c3da59ee245eebb03b6f660a3e8c211f54f985
aocm <aocm.dev@gmail.com>
2022-05-05 11:56:14 +0900

    fix: メニューとrouter周りの処理見直し

--- ssr-server/src/layouts/MemoLayout.vue
+++ ssr-server/src/layouts/MemoLayout.vue
@@ -1,20 +1,13 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { useRouter } from "vue-router"
-
-const activeIndex = ref('1')
-const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
+import { useRouter,useRoute } from "vue-router"
 const router = useRouter()
-const goMemoView = ()=>{
-  router.push('/memo/')
-}
-const goMemoCreate = ()=>{
-  router.push('/memo/new')
-}
-const goTop = ()=>{
-  router.push('/')
+const route = useRoute()
+
+const activeIndex = ref(String(route.path))
+const handleSelect = (key: string, keyPath: string[]) => {
+  // console.log(key, keyPath)
+  window.location.href = key
 }
 </script>
 <template>
@@ -28,9 +21,9 @@ const goTop = ()=>{
       mode="horizontal"
       @select="handleSelect"
     >
-      <el-menu-item index="1" @click="goMemoView">みる</el-menu-item>
-      <el-menu-item index="2" @click="goMemoCreate">つくる</el-menu-item>
-      <el-menu-item index="3" disabled>さがす</el-menu-item>
+      <el-menu-item index="/memo" @click="goMemoView">みる</el-menu-item>
+      <el-menu-item index="/memo/new" @click="goMemoCreate">つくる</el-menu-item>
+      <el-menu-item index="/memo/search" disabled>さがす</el-menu-item>
     </el-menu>
     <el-main>
       <router-view />
